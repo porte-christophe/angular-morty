4cdde9719f3a3653eb5ad380726f23d640584df4
Christophe Porte <porte17christophe69@gmail.com>
2026-07-21 17:23:29 +0200

    affichage des noms

--- src/app/features/characters/services/characters.ts
+++ src/app/features/characters/services/characters.ts
@@ -13,6 +13,8 @@ export class CharactersService {
   private infos = signal<InfoResponse>({} as InfoResponse);
   readonly characterSignal = this.characters.asReadonly();
   readonly url = 'https://rickandmortyapi.com/api/character/';
+   urlVar:string = "";
+  private first = true;
   getInfos(){
     return this.infos;
   }
@@ -21,6 +23,29 @@ export class CharactersService {
       .get<ApiResponse<Character[]>>(this.url)
       .pipe(tap((response: ApiResponse<Character[]>) => {this.infos.set(response.info); }));
   }
+  setUrlResidents(residents: string[]){
+    this.first = true;
+    this.urlVar="";
+    if(residents.length === 0){
+      return this.urlVar;
+    }
+    residents.forEach((resident) =>
+    {
+      if(this.first){
+        this.urlVar += this.url + resident.slice(42);
+        this.first = false;
+      }else{
+        this.urlVar += "," + resident.slice(42);
+      }
+    })
+    return this.urlVar;
+  }
+  getCharacters(url:string, characters: Character[]): Observable<Character[]> {
+    return this.http
+    .get<Character[]>(url)
+    //.pipe(tap((response: Character[]) => {characters.set(response);console.log(response);}));
+  }
+
 
   getCharactersFromService(page: number = 1): Observable<ApiResponse<Character[]>> {
     return this.http
--- src/app/features/planets/components/planet-card/planet-card.ts
+++ src/app/features/planets/components/planet-card/planet-card.ts
@@ -1,5 +1,10 @@
-import { Component, input } from '@angular/core';
+import { Component, input, OnInit, inject, signal } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable, tap } from 'rxjs';
 import { Planet } from '../../types/planet.type';
+import { CharactersService } from '../../../characters/services/characters';
+import { Character } from '../../../characters/types/character.type';
+import { ApiResponse } from '../../../../shared/types/api-response.types';
 
 @Component({
   selector: 'app-planet-card',
@@ -7,6 +12,24 @@ import { Planet } from '../../types/planet.type';
   templateUrl: './planet-card.html',
   styleUrl: './planet-card.css',
 })
-export class PlanetCard {
+export class PlanetCard implements OnInit{
+  private readonly http = inject(HttpClient);
+  private readonly characterService = inject(CharactersService);
   planet = input.required<Planet>();
+  private url:string = "";
+  characters: Character[] = [{id:87848, name: "zefezf"}as Character];
+  ngOnInit() {
+    
+    
+  }
+  
+  getChar(){
+    this.url = this.characterService.setUrlResidents(this.planet().residents);
+    if (this.url!="") {
+      this.characterService.getCharacters(this.url, this.characters).subscribe((c: Character[]) => {
+        this.characters = c; console.log(this.characters);});
+
+    }
+  }
+
 }
